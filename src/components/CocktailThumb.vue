9bf4125c7db7c6ccdf5e5b228bbfca2fdad98dcd
Marina Yanysheva <yme@incom-sr.ru>
2023-11-27 16:46:15 +0300

    deploy app

---
+++ src/components/CocktailThumb.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="root">
+		<RouterLink :to="`/cocktails/${cocktail.idDrink}`">
+			<div
+				:style="`background-image: url(${cocktail.strDrinkThumb})`"
+				class="pic"
+			></div>
+		</RouterLink>
+		<div class="name">{{ cocktail.strDrink }}</div>
+	</div>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+	cocktail: {
+		type: Object,
+		required: true,
+	},
+});
+</script>
+
+<style lang="sass" scoped>
+@import '../assets/styles/main'
+.root
+    width: calc((100% - 76px) / 3)
+    margin-right: 38px
+    padding-bottom: 20px
+    &:nth-child(3n + 3)
+        margin-right: 0
+
+.pic
+    width: 120px
+    height: 120px
+    margin: 0 auto
+    background-repeat: no-repeat
+    background-position: 50% 50%
+    background-size: 100%
+
+.name
+    padding-top: 15px
+    width: 100%
+    white-space: nowrap
+    overflow: hidden
+    text-overflow: ellipsis
+    letter-spacing: 0.1em
+</style>
