68f0408d819d8209f37b5c9a567b62508925ab9a
Marina Yanysheva <yme@incom-sr.ru>
2023-11-28 08:29:52 +0300

    fix base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: `/cocktails/`,
+  base: '/cocktails/',
 	plugins: [vue()],
 	resolve: {
 		alias: {
